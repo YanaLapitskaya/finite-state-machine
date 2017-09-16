6c05e3743563516fb84730c290ec6a8386a833cd
Yana <yanalapitskay@gmail.com>
2017-09-16 22:16:51 +0100

    solution

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,60 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.states=config.states;
+        this.curState=config.initial;
+        //history
+        this.histArray=[this.curState];
+        this.tailIndex=0;
+        this.wasCalled;
+        this.redoDisabled;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.curState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this.states.hasOwnProperty(state)){
+            this.curState=state;
+            this.histArray.push(this.curState);
+            this.redoDisabled=true;
+        }
+        else throw new Error();
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        for(let transition in this.states[this.curState].transitions){
+            if(transition===event){
+                this.curState=this.states[this.curState].transitions[event];
+                this.histArray.push(this.curState);
+                this.redoDisabled=true;
+                return;
+            }
+        }
+        throw new Error();
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.curState="normal";
+        this.histArray=[this.curState];
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,28 +64,93 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let ar=[];
+        if(arguments.length!=0){
+            for(let stateName in this.states){
+                if(this.states[stateName].transitions.hasOwnProperty(event)){
+                    ar.push(stateName);                }
+            }
+        }
+        else
+            for(let stateName in this.states){
+                ar.push(stateName);
+            }
+
+        return ar;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if((this.histArray.length-this.tailIndex)>1){
+            this.tailIndex++;
+            this.curState=this.histArray[this.histArray.length-1-this.tailIndex];
+            this.redoDisabled=false;
+            return true;
+        }
+        else return false;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if((!this.redoDisabled)&&(this.tailIndex>0)) {
+                this.tailIndex--;
+                this.curState=this.histArray[this.histArray.length-1-this.tailIndex];
+                return true;
+        }
+        else return false;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {this.histArray=[];this.tailIndex=0;}
 }
 
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
+const config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+
+
+(function(){
+
+            const student = new FSM(config);
+
+            student.trigger('study');
+            console.log(student.getState()+'busy');
+})()
